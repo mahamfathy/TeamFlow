d1596157972daa90cea082c85bc6b2a518ccbd17
mahamfathy <mahafathyh@gmail.com>
2025-07-23 00:45:33 +0300

    [feat] component  : create card components

---
+++ src/app/feature/dashboard/modules/team/components/team-member-card/team-member-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-member-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './team-member-card.component.html',
+  styleUrl: './team-member-card.component.scss'
+})
+export class TeamMemberCardComponent {
+
+}
