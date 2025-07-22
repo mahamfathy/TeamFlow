e6f17bbf35c33f5164c99fd45fa3e48474d4bcf9
mahamfathy <mahafathyh@gmail.com>
2025-07-22 23:16:15 +0300

    [feat] component  : edits in  component

--- src/app/shared/components/side-nav-bar/side-nav-bar.component.ts
+++ src/app/shared/components/side-nav-bar/side-nav-bar.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav-bar',
-  standalone: true,
-  imports: [],
+  standalone: false,
   templateUrl: './side-nav-bar.component.html',
-  styleUrl: './side-nav-bar.component.scss'
+  styleUrl: './side-nav-bar.component.scss',
 })
-export class SideNavBarComponent {
-
-}
+export class SideNavBarComponent {}
