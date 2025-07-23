bf1332f19478f836f0a16df6d09561927261ea26
mahamfathy <mahafathyh@gmail.com>
2025-07-23 22:23:20 +0300

    [feat] sideNavbar component  : create linkClicked Output

--- src/app/shared/components/side-nav-bar/side-nav-bar.component.ts
+++ src/app/shared/components/side-nav-bar/side-nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-side-nav-bar',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './side-nav-bar.component.html',
   styleUrl: './side-nav-bar.component.scss',
 })
-export class SideNavBarComponent {}
+export class SideNavBarComponent {
+  @Output() linkClicked = new EventEmitter<void>();
+}
